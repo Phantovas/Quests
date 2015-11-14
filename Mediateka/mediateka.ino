a2e81a896b190c60e29bbcf60e1e420bdb795ba6
Vasiliy Ponomarjov <vas@vingrad.ru>
2015-11-14 21:44:15 +0300

    Update state medialeka

--- Mediateka/mediateka.ino
+++ Mediateka/mediateka.ino
@@ -5,7 +5,7 @@
  * @email vas@vingrad.ru
 */
 
-//#define DEBUG
+#define DEBUG
 
 /**
 * Системные модули
@@ -31,12 +31,76 @@ void setup() {
 #ifdef DEBUG
   Serial.begin(9600);
 #endif
-  state = ST_START;
-}
+  //состояния входных пинов
+  pinMode(INDOOR_PIN, INPUT);
+  digitalWrite(INDOOR_PIN, HIGH);
+  
+  //состояния выходных пинов
+  pinMode(LIGHTING_PIN, OUTPUT);
+  digitalWrite(LIGHTING_PIN, LOW);
+  pinMode(TV_PIN, OUTPUT);
+  digitalWrite(TV_PIN, LOW);
+  pinMode(PHONE_PIN, OUTPUT);
+  digitalWrite(PHONE_PIN, LOW);
+  pinMode(BOX_PIN, OUTPUT);
+  digitalWrite(BOX_PIN, LOW);
+  pinMode(OUTDOOR_PIN, OUTPUT);
+  digitalWrite(OUTDOOR_PIN, LOW);
+  //запускаем режим ожидания
+  state = ST_WAIT;
+}  
 
 /**
  * Основная функция 
  */
 void loop() {
-  
+#ifdef DEBUG
+  Serial.print("state ");
+  Serial.println(state);
+#endif
+  switch (state) {
+    case ST_WAIT: {
+      //если в режиме ожидания открыли дверь, то переходим в состояние 
+      //включение света
+      if (digitalRead(INDOOR_PIN) == HIGH) {
+        digitalWrite(LIGHTING_PIN, HIGH); //включаем освещение
+        changeState(ST_LIGHTING);
+      }
+      break;
+    };
+    case ST_LIGHTING: {
+#ifdef DEBUG
+  Serial.print("time ");
+  Serial.println(millis() - start_time);
+  Serial.println(timing[state]*1000);
+  Serial.println(millis() - start_time >  timing[state]*1000);
+#endif
+      if (millis() - start_time > timing[state]*1000) {
+        digitalWrite(TV_PIN, HIGH);
+        changeState(ST_TV_ON);
+      }
+      break;
+    };
+    case ST_TV_ON: {
+      break; 
+    };
+    case ST_PHONE: {
+      break;
+    };
+    case ST_PHOTOCELL: {
+      break;
+    };
+    case ST_GAME_OVER: {
+      state = ST_WAIT;
+    }
+  }
+}
+
+/**
+ * Функция изменения состояния с фиксирование времени
+ * @param _state
+ */
+void changeState(TState _state) {
+  start_time = millis(); //время включения
+  state = _state; //меняем состояние  
 }
--- Mediateka/vars.h
+++ Mediateka/vars.h
@@ -9,6 +9,7 @@
 #define MP3_RING 1 //мелодия звонка телефона
 #define MP3_TEXT 2 //мелодия текста
 #define MP3_BUSY 3 //мелодия занятия телефона
+#define MP3_BOX  4 //мелодия шкатулки
 /******************************************************************************/
 
 /******************** КОНТАКТЫ ************************************************/
@@ -16,16 +17,27 @@
 #define BUSY 13 //LOW во время воспроизведения
 #define S_RX 9 //RX for management player
 #define S_TX  10 // TX for management player
-//пин для двери
-#define DOOR_PIN 4 //пин для открытия двери
-#define TV_PIN 3 //пин для включения ТВ
+//пин входных состояний
+#define INDOOR_PIN 2 //пин для открытия двери
+#define PHOTOCELL_PIN 14 //пин для подключения фотоэлемента
+//пины кправления механизмами
+#define LIGHTING_PIN 3 //пин освещения
+#define TV_PIN 4 //пин для включения ТВ
 #define PHONE_PIN 5 //пин поднятия трубки
-#define PHOTOCELL_PIN 6 
+#define BOX_PIN 6 //пин открытия шкатулки
+#define OUTDOOR_PIN 7 //пин открытия первой двери
 
 //плеер
 SoftwareSerial mSerial(S_RX, S_TX);      //Инициализация софтверного сериала для управления звуком
 
 //состояния игры (запуск, освещение, включение ТВ, звонок, подсветка фотоэлемента, конец игры)
-enum {ST_START, ST_LIGHTING, ST_TV_ON, ST_PHONE, ST_PHOTOCELL, ST_GAME_OVER} state; 
+typedef enum {ST_WAIT, ST_LIGHTING, ST_TV_ON, ST_PHONE, ST_PHOTOCELL, ST_GAME_OVER} TState; 
 
+TState state;
+
+//времена работы состояний
+unsigned long timing[6] = {0, 120, 105, 0, 0, 0};
+
+//время запуска события
+unsigned long start_time = 0;
 
