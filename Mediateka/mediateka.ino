d5f686e702811b08c2a6d62ae78db7bad9faaba4
Vasiliy Ponomarjov <vas@vingrad.ru>
2015-11-15 18:15:55 +0300

    MP3 phone

--- Mediateka/mediateka.ino
+++ Mediateka/mediateka.ino
@@ -17,7 +17,6 @@
 #include <Bounce2.h>
 #include <SoftwareSerial.h>
 #include <DFPlayer_Mini_Mp3.h>
-#include <OneWire.h>
 
 /**
  * Модули
@@ -30,22 +29,34 @@
 void setup() {
 #ifdef DEBUG
   Serial.begin(9600);
+  Serial.println("started");
 #endif
-  //состояния входных пинов
+  //инициализация входных пинов
+  pinMode(BUSY, INPUT);
   pinMode(INDOOR_PIN, INPUT);
+  pinMode(PHONE_PIN, INPUT);
+  //состояния входных пинов
+  digitalWrite(BUSY, HIGH);
   digitalWrite(INDOOR_PIN, HIGH);
+  digitalWrite(PHONE_PIN, HIGH);
+  //активируем антидребезг
+  phoneBtn.attach(PHONE_PIN);
+  phoneBtn.interval(TIME_BOUNCE_PROGRAM);
   
-  //состояния выходных пинов
+  //инициализация выходных пинов
   pinMode(LIGHTING_PIN, OUTPUT);
-  digitalWrite(LIGHTING_PIN, LOW);
   pinMode(TV_PIN, OUTPUT);
-  digitalWrite(TV_PIN, LOW);
-  pinMode(PHONE_PIN, OUTPUT);
-  digitalWrite(PHONE_PIN, LOW);
   pinMode(BOX_PIN, OUTPUT);
-  digitalWrite(BOX_PIN, LOW);
   pinMode(OUTDOOR_PIN, OUTPUT);
+  //состояния выходных пинов
+  digitalWrite(LIGHTING_PIN, LOW);
+  digitalWrite(TV_PIN, LOW);
+  digitalWrite(BOX_PIN, LOW);
   digitalWrite(OUTDOOR_PIN, LOW);
+  //плеер
+	mSerial.begin(9600);
+	mp3_set_serial(mSerial); 
+  mp3_stop(); //вдруг жестко перенрузили на произрывании музыки
   //запускаем режим ожидания
   state = ST_WAIT;
 }  
@@ -55,45 +66,78 @@ void setup() {
  */
 void loop() {
 #ifdef DEBUG
-  Serial.print("state ");
-  Serial.println(state);
+//  Serial.print("state ");
+//  Serial.println(state);
 #endif
   switch (state) {
-    case ST_WAIT: {
+    case ST_WAIT: 
       //если в режиме ожидания открыли дверь, то переходим в состояние 
       //включение света
       if (digitalRead(INDOOR_PIN) == HIGH) {
-        digitalWrite(LIGHTING_PIN, HIGH); //включаем освещение
+        //включаем освещение 
+        digitalWrite(LIGHTING_PIN, HIGH); 
         changeState(ST_LIGHTING);
       }
       break;
-    };
-    case ST_LIGHTING: {
-#ifdef DEBUG
-  Serial.print("time ");
-  Serial.println(millis() - start_time);
-  Serial.println(timing[state]*1000);
-  Serial.println(millis() - start_time >  timing[state]*1000);
-#endif
+    case ST_LIGHTING: 
+      //освещение горит в течение заданного времени (массив timing в vars.h)
       if (millis() - start_time > timing[state]*1000) {
+        //по прошествию заданного времени включается ТВ
         digitalWrite(TV_PIN, HIGH);
         changeState(ST_TV_ON);
       }
       break;
-    };
-    case ST_TV_ON: {
+    case ST_TV_ON: 
+      //показываем видеофрагмент в течение заданного времени (массив timing в vars.h)
+      if (millis() - start_time > timing[state]*1000) {
+        changeState(ST_PHONE);
+      }
       break; 
-    };
-    case ST_PHONE: {
+    case ST_PHONE:
+      //если телефон не в состоянии ЗАНЯТО, то проверяем состояние трубки
+      //иначе ничего не делаем, у нас и так по-умолчанию проверка стоит на 
+      //снятую трубку
+      if (phoneState != PN_BUSY) {
+        //если в режиме звонка
+        if (phoneState == PN_RING) {
+          phoneMelody = MP3_RING; 
+          if (phoneBtn.update()) {
+            //если подняли трубку
+            if (phoneBtn.read() == LOW) {
+              phoneMelody = MP3_TEXT; 
+              phoneState = PN_PLAY;
+              mp3_stop();
+            }  
+          }
+          play(phoneMelody);  
+        } else {
+          Serial.print("digitalRead(BUSY) ");
+          Serial.println(digitalRead(BUSY));
+          if (digitalRead(BUSY)) {
+            mp3_stop();
+            changeState(ST_PHOTOCELL);
+          }
+        }
+      } else {
+        //тут можно привлечь внимание к телефону
+        //может лежит неправильно трубка или ее вообще сняли 
+        //ну и воспроизводим занято :)
+        phoneBusy(); 
+      }
       break;
-    };
-    case ST_PHOTOCELL: {
+    case ST_PHOTOCELL: 
       break;
-    };
-    case ST_GAME_OVER: {
+    case ST_GAME_OVER: 
+      mp3_stop(); //вдруг жестко перенрузили на произрывании музыки
       state = ST_WAIT;
-    }
   }
+  //**************************************************************************
+  //ДЛЯ СИЛЬНО ДОТОШНЫХ, ЕСЛИ СХВАТЯТ ТЕЛЕФОН В ЛЮБОЙ МОМЕНТ 
+  //БУДЕТ ИГРАТЬ МЕЛОДИЯ ЗАНЯТО :))
+  //**************************************************************************
+  if (state != ST_PHONE) {
+    phoneBusy();
+  } 
 }
 
 /**
@@ -101,6 +145,37 @@ void loop() {
  * @param _state
  */
 void changeState(TState _state) {
+#ifdef DEBUG
+      Serial.print("change state");
+      Serial.println(_state);
+#endif
   start_time = millis(); //время включения
   state = _state; //меняем состояние  
+}
+
+/**
+ * Функция воспроизведения звука
+ * @param _melody номер мелодии
+ */
+void play(unsigned int _melody) {
+  delay(DELAY_MP3_PLAY);
+  if (digitalRead(BUSY)) {
+    mp3_play(_melody);
+  }  
+}
+
+/**
+ * Функция проверки состояния телефона
+ */
+void phoneBusy() {
+    phoneBtn.update();
+    //состояние телефона
+    if (phoneBtn.read() == HIGH) {
+      phoneState = PN_RING;
+      mp3_stop();
+    } else {
+      phoneState = PN_BUSY;
+      //играем в телефон занято
+      play(MP3_BUSY);
+    }     
 }
--- Mediateka/vars.h
+++ Mediateka/vars.h
@@ -5,9 +5,13 @@
  * @email vas@vingrad.ru
 */
 
+//общие константы
+#define TIME_BOUNCE_PROGRAM      500           //время реакции кнопки на режим программирования
+#define DELAY_MP3_PLAY           50            //задержка на воспроизведение звука
+
 /***************НОМЕРА МЕЛОДИЙ*************************************************/
 #define MP3_RING 1 //мелодия звонка телефона
-#define MP3_TEXT 2 //мелодия текста
+#define MP3_TEXT 6 //мелодия текста
 #define MP3_BUSY 3 //мелодия занятия телефона
 #define MP3_BOX  4 //мелодия шкатулки
 /******************************************************************************/
@@ -19,11 +23,11 @@
 #define S_TX  10 // TX for management player
 //пин входных состояний
 #define INDOOR_PIN 2 //пин для открытия двери
+#define PHONE_PIN 3 //пин поднятия трубки
 #define PHOTOCELL_PIN 14 //пин для подключения фотоэлемента
 //пины кправления механизмами
-#define LIGHTING_PIN 3 //пин освещения
-#define TV_PIN 4 //пин для включения ТВ
-#define PHONE_PIN 5 //пин поднятия трубки
+#define LIGHTING_PIN 4 //пин освещения
+#define TV_PIN 5 //пин для включения ТВ
 #define BOX_PIN 6 //пин открытия шкатулки
 #define OUTDOOR_PIN 7 //пин открытия первой двери
 
@@ -32,12 +36,17 @@ SoftwareSerial mSerial(S_RX, S_TX);      //Инициализация софтв
 
 //состояния игры (запуск, освещение, включение ТВ, звонок, подсветка фотоэлемента, конец игры)
 typedef enum {ST_WAIT, ST_LIGHTING, ST_TV_ON, ST_PHONE, ST_PHOTOCELL, ST_GAME_OVER} TState; 
-
 TState state;
 
-//времена работы состояний
-unsigned long timing[6] = {0, 120, 105, 0, 0, 0};
+//времена работы состояний {0, 120, 105, 0, 0, 0}
+unsigned long timing[6] = {0, 5, 3, 0, 0, 0};
 
 //время запуска события
 unsigned long start_time = 0;
 
+//антидребезг кнопок
+Bounce phoneBtn; //кнопка поднятия телефонной трубки
+
+//состояния телефона
+enum {PN_RING, PN_PLAY, PN_BUSY} phoneState;
+unsigned int phoneMelody = MP3_RING;
